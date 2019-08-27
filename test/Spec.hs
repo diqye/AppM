edd308c2a9b8bffdeaf5a7c319a585d3e7c19851
diqye <diqye@msydeMacBook-Pro.local>
2019-08-27 19:28:07 +0800

    add base test fun

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+import Web.AppM
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Test suite not yet implemented"
+
+testApp :: Application -> Request -> (Response -> IO Bool) -> IO Bool
+testApp req app test = do
+  mr <- newEmptyMVar
+  app req $ \ resp -> test resp >>= putMar mr >> pure ResponseReceived
+  r <- takeMVar mr
+  return r
+
+
+
+
